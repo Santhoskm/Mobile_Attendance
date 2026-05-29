7e1aac5776be828a3606730dfffc7412e38148c2
Santhoskm <kmsanthosh336@gmail.com>
2026-05-29 16:01:04 +0530

    updated new

--- attendance-app/myapp/models.py
+++ attendance-app/myapp/models.py
@@ -46,7 +46,7 @@ class User(AbstractUser):
     sidemenustatus=models.IntegerField(default='0')
     workerrequiredstatus=models.IntegerField(default='0')
     ppestatus=models.IntegerField(default='0')
-    empid = models.CharField(max_length=200,default='')
+    empid = models.CharField(max_length=200,unique=True,null=True,blank=True)
 
 
 class EmployeeRegistrationWorkforce(models.Model):
